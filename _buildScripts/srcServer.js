d54167f7ade7ab1a186173905be99099fe5985bc
ahmed-abdelmotey <ahmed.abdelmotey92@gmail.com>
2018-07-12 15:03:05 +0200

    Now Browser sync run with expres, also scripts run silently

--- _buildScripts/srcServer.js
+++ _buildScripts/srcServer.js
@@ -1,19 +1,40 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
-var open = require('open');
+var cors = require('cors');
 
 var port = 3000;
 
 var app = express();
 
-app.get('/', function(req,res){
-	res.sendFile(path.join(__dirname, '../src/index.html'));
-})
+app.set('port', process.env.PORT || port);
 
-app.listen(port, function(err){
-	if(err){
-		console.log(err);
-	} else {
-		open('http://localhost:' + port);
-	}
+var browserSync = require('browser-sync');
+var bsconf = {
+		host: 'localhost',
+		port: 3001,
+		https: false,
+		notify: false,
+		open: false,
+		online: false,
+		ui: false,
+		server: {
+				baseDir: path.join(__dirname, '../src')
+		},
+		files: [path.join(__dirname, '../src/**/*.{html,css,js,json}')]
+};
+var bs = browserSync.create().init(bsconf);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../src')));
+app.use(cors());
+app.use(require('connect-browser-sync')(bs));
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(app.get('port'), function(err){
+	if(err){ 	console.log(err); }
 })
--- _buildScripts/startMessage.js
+++ _buildScripts/startMessage.js
@@ -1,3 +1,16 @@
 var chalk = require('chalk');
+var os = require('os');
+
+
+
+var networkInterfaces = os.networkInterfaces( );
+
+var interfaces = networkInterfaces['Wi-Fi'].reduce((acc,cur) => {
+	acc [cur.family] = cur.address;
+	return acc;
+}, {})
+
+var ip =  interfaces['IPv4'] ;
 
 console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.white('[External link]'), chalk.cyan('http://' + ip + ':3001'));
